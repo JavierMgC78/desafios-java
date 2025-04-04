e30861cf993e1232ad5a6401574ffbb0433a139f
JAVIER ALBERTO MEDINA <javier.amgc@outlook.com>
2025-04-03 20:27:32 -0600

    correccion menor en para dar formato a la salida

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; //Para leer datos del usuario.
 
 public class Main {
     public static void main(String[] args) {
@@ -9,27 +9,28 @@ public class Main {
 
         System.out.println(saludo);
 
-        Scanner scanner = new Scanner(System.in); // Creamos el scanner
+        Scanner scanner = new Scanner(System.in); // nos preparamos a leer datos del teclado con System.in
         String respuesta;
 
         System.out.println("Desea continuar? (Y o N):");
         respuesta = scanner.nextLine().trim().toUpperCase();
 
         if (respuesta.equals("Y")) {
-            System.out.println("introduce lo grados celcius: ");
+            System.out.println("Introduce los grados Celsius: ");
             double celsius = scanner.nextDouble();
-            scanner.nextLine(); // Limpiar buffer después de nextInt()
+            scanner.nextLine(); // Limpiar buffer después de nextDouble()
 
             // Conversión de Celsius a Fahrenheit
             double fahrenheit = (celsius * 1.8) + 32;
 
-            System.out.println("Los " + celsius + "°C equivalen a " + fahrenheit + "°F.");
+            // Usamos printf para dar formato a los dsatos
+            System.out.printf("Los %.2f°C equivalen a %.2f°F.%n", celsius, fahrenheit);
 
 
         } else if (respuesta.equals("N")) {
             System.out.println("Has elegido salir. ¡Hasta la próxima!");
         } else {
-            System.out.println("Respuesta no válida. Usa (Y) parea si o (N) para noy.");
+            System.out.println("Respuesta no válida. Usa (Y) para si o (N) para no.");
         }
         scanner.close(); // Cerramos el scanner
 
